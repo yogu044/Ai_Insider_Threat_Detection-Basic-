798892eeac159e7c70ef694bcc618158b039d673
YOGESH D <169351684+yogu044@users.noreply.github.com>
2025-07-19 20:07:55 +0530

    Update test_predict.py

--- backend/test_predict.py
+++ backend/test_predict.py
@@ -3,9 +3,9 @@ import requests
 
 test_users = [
     {"name": "Yogesh", "action": "login", "timestamp": "2025-04-26 03:15:00"},   
-    {"name": "Anita", "action": "access_file", "timestamp": "2025-04-26 14:30:00"}, # Normal
-    {"name": "Vikram", "action": "delete_file", "timestamp": "2025-04-26 02:45:00"}, # Suspicious (midnight)
-    {"name": "Neha", "action": "login", "timestamp": "2025-04-26 12:00:00"},       # Normal
+    {"name": "Anita", "action": "access_file", "timestamp": "2025-04-26 14:30:00"},
+    {"name": "Vikram", "action": "delete_file", "timestamp": "2025-04-26 02:45:00"}, 
+    {"name": "Neha", "action": "login", "timestamp": "2025-04-26 12:00:00"},       
 ]
 
 
